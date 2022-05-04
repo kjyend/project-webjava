739e1095580ac4abe0366d7c9aeebc4d13325282
junyoung kim <adad568@naver.com>
2022-05-04 18:15:55 +0900

    first-commit

---
+++ src/main/java/clothes/clothesproject/ClothesprojectApplication.java
@@ -0,0 +1,13 @@
+package clothes.clothesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesprojectApplication.class, args);
+	}
+
+}
